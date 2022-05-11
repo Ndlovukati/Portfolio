d76361f5fe3cb5773e5bb4c324819a7c27566c59
Ndlovukati <ndlovukati1123@gmail.com>
2022-05-11 15:53:31 +0200

    fixing server.js

--- frontend/portfolio/server.js
+++ frontend/portfolio/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(dirname + '/dist/portfolio'));
+app.use(express.static(__dirname + '/dist/portfolio'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(dirname+
+res.sendFile(path.join(__dirname+
 '/dist/portfolio/index.html'));});
-app.listen(process.env.PORT || 4305);
+app.listen(process.env.PORT || 4200);
